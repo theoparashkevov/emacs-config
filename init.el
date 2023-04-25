59415e00a09db2c62e434ae9bf47d50752bb755c
theoparashkevov <theo.parashkevov@gmail.com>
2023-04-25 18:36:42 +0300

    Updated to general init.el

--- init.el
+++ init.el
@@ -2,4 +2,21 @@
 (load-theme 'modus-vivendi )
 (setq modus-themes-mode-line 'accented)
 
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "init.org" "/home/tcv/.config/emacs/org/")
+ )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company-jedi company py-snippets yasnippet-snippets flycheck python-mode yasnippet use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
