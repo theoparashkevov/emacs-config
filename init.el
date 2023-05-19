4f018a017135a32375bd5f44e7ef5a1163629beb
theoparashkevov <theo.parashkevov@gmail.com>
2023-05-19 08:47:45 +0300

    Added ORG Shift Select/ Rearranged Company backends

--- init.el
+++ init.el
@@ -7,13 +7,15 @@
  (expand-file-name "init.org" "/home/tcv/.config/emacs/org/")
  )
 
+(setq org-support-shift-select t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company-jedi company py-snippets yasnippet-snippets flycheck python-mode yasnippet use-package)))
+   '(lsp-jedi jedi company-shell company-jedi company py-snippets yasnippet-snippets flycheck python-mode yasnippet use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
